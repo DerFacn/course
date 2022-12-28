866d5edab49bd74f89c237a7ed851efc17b22449
derfacn <nikitalupalo145@gmail.com>
2022-12-28 18:55:57 +0100

    secret key hided

--- application/main/settings.py
+++ application/main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q6w-f)3lt7cs+qignih=a-3s!&a@-kzh!ftm@s8t_23u)vq3*b"
+SECRET_KEY = "aaa"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
